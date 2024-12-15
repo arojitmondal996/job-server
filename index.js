1d011dd20cee59e033c57714903047860e383858
arojitmondal996e <arojitmondal996@gmail.com>
2024-12-15 14:29:13 +0600

    some doc updated

--- index.js
+++ index.js
@@ -23,6 +23,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    // 111111111 prothome ekta connection banate hobe
+    const db = client.db('solo-db')
+    const jobsCollection = db.collection('jobs')
+
+    // 222222222 sava jobData in database
+    app.post('/add-job', async(req, res) => {
+      // client side theke data ta pathale eakhane res.body er madhome dhorbo
+      const jobData = req.body
+      const result = await jobsCollection.insertOne(jobData)
+      console.log(result)
+      res.send(result)
+    })
+
+    // 33333333333 get er madhome data ta dekhate hobe
+    app.get('/jobs', async(req, res) => {
+      const result = await jobsCollection.find().toArray()
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
